541cdf8c92d3a0cfbc0ee4a6988bad9d62cd72c8
Qing Ma <qing.ma@isatis-health.nl>
2019-09-28 10:19:46 +0200

    CUD books

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -33,7 +33,13 @@ export class DashboardComponent implements OnInit {
   }
 
   deleteBook(bookID: number): void {
-    console.warn(`Delete book not yet implemented (bookID: ${bookID}).`);
+    this.dataService.deleteBook(bookID).subscribe(
+      (data: void) => {
+        const index = this.allBooks.findIndex(book => book.bookID === bookID);
+        this.allBooks.splice(index, 1);
+      },
+      (error: any) => console.log(error)
+    );
   }
 
   deleteReader(readerID: number): void {
--- src/app/edit-book/edit-book.component.ts
+++ src/app/edit-book/edit-book.component.ts
@@ -33,6 +33,9 @@ export class EditBookComponent implements OnInit {
   }
 
   saveChanges(): void {
-    console.warn('Save changes to book not yet implemented.');
+    this.dataService.updateBook(this.selectedBook).subscribe(
+      (data: void) => console.log(`${this.selectedBook.title} updated successfully`),
+      (error: any) => console.log(error)
+    );
   }
 }
